41326636dd5d0c333e368144f528aa8a7748d45d
Ajay S <ajayshivaji16@gmail.com>
2024-09-03 13:43:40 +0530

    Update app.py

--- movie_recommender_system-main/app.py
+++ movie_recommender_system-main/app.py
@@ -4,44 +4,47 @@ import requests
 import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
-movies=pd.read_csv('movie_recommender_system-main/dataset.csv')
-movies.isnull().sum()
-movies=movies[['id', 'title', 'overview', 'genre']]
-movies['tags'] = movies['overview']+movies['genre']
-new_data  = movies.drop(columns=['overview', 'genre'])
 
-cv=CountVectorizer(max_features=10000, stop_words='english')
-vector=cv.fit_transform(new_data['tags'].values.astype('U')).toarray()
-similarity=cosine_similarity(vector)
-def recommand(movies):
-    index=new_data[new_data['title']==movies].index[0]
-    distance = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda vector:vector[1])
-    for i in distance[0:5]:
-        print(new_data.iloc[i[0]].title)
+# Load and preprocess the dataset
+movies = pd.read_csv('movie_recommender_system-main/dataset.csv')
+movies = movies[['id', 'title', 'overview', 'genre']]
+movies['tags'] = movies['overview'] + movies['genre']
+new_data = movies.drop(columns=['overview', 'genre'])
 
+# Feature extraction using CountVectorizer
+cv = CountVectorizer(max_features=10000, stop_words='english')
+vector = cv.fit_transform(new_data['tags'].values.astype('U')).toarray()
+
+# Calculate similarity matrix
+similarity = cosine_similarity(vector)
+
+# Save the processed data
 pickle.dump(new_data, open('movies_list.pkl', 'wb'))
 pickle.dump(similarity, open('similarity.pkl', 'wb'))
-pickle.load(open('movies_list.pkl', 'rb'))
 
-def fetch_poster(movie_id):
-     url = "https://api.themoviedb.org/3/movie/{}?api_key=c7ec19ffdd3279641fb606d19ceb9bb1&language=en-US".format(movie_id)
-     data=requests.get(url)
-     data=data.json()
-     poster_path = data['poster_path']
-     full_path = "https://image.tmdb.org/t/p/w500/"+poster_path
-     return full_path
-
+# Load the processed data
 movies = pickle.load(open("movies_list.pkl", 'rb'))
 similarity = pickle.load(open("similarity.pkl", 'rb'))
-movies_list=movies['title'].values
+movies_list = movies['title'].values
 
+# Function to fetch movie poster
+def fetch_poster(movie_id):
+    url = "https://api.themoviedb.org/3/movie/{}?api_key=c7ec19ffdd3279641fb606d19ceb9bb1&language=en-US".format(movie_id)
+    data = requests.get(url)
+    data = data.json()
+    poster_path = data['poster_path']
+    full_path = "https://image.tmdb.org/t/p/w500/" + poster_path
+    return full_path
+
+# Streamlit app header
 st.header("Movie Recommender System")
-#movie_recommender_system-main\app.py
+
+# Image Carousel Component
 import streamlit.components.v1 as components
 
 imageCarouselComponent = components.declare_component("image-carousel-component", path=r"frontend\public")
 
-
+# Sample image URLs for carousel
 imageUrls = [
     fetch_poster(1632),
     fetch_poster(299536),
@@ -56,29 +59,29 @@ imageUrls = [
     fetch_poster(914),
     fetch_poster(255709),
     fetch_poster(572154)
-   
-    ]
-
+]
 
 imageCarouselComponent(imageUrls=imageUrls, height=200)
-selectvalue=st.selectbox("Select movie from dropdown", movies_list)
 
+# Movie selection dropdown
+selectvalue = st.selectbox("Select movie from dropdown", movies_list)
+
+# Recommendation function
 def recommend(movie):
-    index=movies[movies['title']==movie].index[0]
-    distance = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda vector:vector[1])
-    recommend_movie=[]
-    recommend_poster=[]
+    index = movies[movies['title'] == movie].index[0]
+    distance = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda vector: vector[1])
+    recommend_movie = []
+    recommend_poster = []
     for i in distance[1:6]:
-        movies_id=movies.iloc[i[0]].id
+        movies_id = movies.iloc[i[0]].id
         recommend_movie.append(movies.iloc[i[0]].title)
         recommend_poster.append(fetch_poster(movies_id))
     return recommend_movie, recommend_poster
 
-
-
+# Display recommendations when button is clicked
 if st.button("Show Recommend"):
     movie_name, movie_poster = recommend(selectvalue)
-    col1,col2,col3,col4,col5=st.columns(5)
+    col1, col2, col3, col4, col5 = st.columns(5)
     with col1:
         st.text(movie_name[0])
         st.image(movie_poster[0])
