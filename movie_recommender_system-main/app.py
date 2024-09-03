85a4d2d9a25cec3078723786537b0a1be96a774b
Ajay S <ajayshivaji16@gmail.com>
2024-09-03 13:07:37 +0530

    Add files via upload

---
+++ movie_recommender_system-main/app.py
@@ -0,0 +1,96 @@
+import streamlit as st
+import pickle
+import requests
+import pandas as pd
+
+movies=pd.read_csv('dataset.csv')
+movies.isnull().sum()
+movies=movies[['id', 'title', 'overview', 'genre']]
+movies['tags'] = movies['overview']+movies['genre']
+new_data  = movies.drop(columns=['overview', 'genre'])
+from sklearn.feature_extraction.text import CountVectorizer
+cv=CountVectorizer(max_features=10000, stop_words='english')
+vector=cv.fit_transform(new_data['tags'].values.astype('U')).toarray()
+from sklearn.metrics.pairwise import cosine_similarity
+similarity=cosine_similarity(vector)
+def recommand(movies):
+    index=new_data[new_data['title']==movies].index[0]
+    distance = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda vector:vector[1])
+    for i in distance[0:5]:
+        print(new_data.iloc[i[0]].title)
+
+pickle.dump(new_data, open('movies_list.pkl', 'wb'))
+pickle.dump(similarity, open('similarity.pkl', 'wb'))
+pickle.load(open('movies_list.pkl', 'rb'))
+
+def fetch_poster(movie_id):
+     url = "https://api.themoviedb.org/3/movie/{}?api_key=c7ec19ffdd3279641fb606d19ceb9bb1&language=en-US".format(movie_id)
+     data=requests.get(url)
+     data=data.json()
+     poster_path = data['poster_path']
+     full_path = "https://image.tmdb.org/t/p/w500/"+poster_path
+     return full_path
+
+movies = pickle.load(open("movies_list.pkl", 'rb'))
+similarity = pickle.load(open("similarity.pkl", 'rb'))
+movies_list=movies['title'].values
+
+st.header("Movie Recommender System")
+#movie_recommender_system-main\app.py
+import streamlit.components.v1 as components
+
+imageCarouselComponent = components.declare_component("image-carousel-component", path=r"frontend\public")
+
+
+imageUrls = [
+    fetch_poster(1632),
+    fetch_poster(299536),
+    fetch_poster(17455),
+    fetch_poster(2830),
+    fetch_poster(429422),
+    fetch_poster(9722),
+    fetch_poster(13972),
+    fetch_poster(240),
+    fetch_poster(155),
+    fetch_poster(598),
+    fetch_poster(914),
+    fetch_poster(255709),
+    fetch_poster(572154)
+   
+    ]
+
+
+imageCarouselComponent(imageUrls=imageUrls, height=200)
+selectvalue=st.selectbox("Select movie from dropdown", movies_list)
+
+def recommend(movie):
+    index=movies[movies['title']==movie].index[0]
+    distance = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda vector:vector[1])
+    recommend_movie=[]
+    recommend_poster=[]
+    for i in distance[1:6]:
+        movies_id=movies.iloc[i[0]].id
+        recommend_movie.append(movies.iloc[i[0]].title)
+        recommend_poster.append(fetch_poster(movies_id))
+    return recommend_movie, recommend_poster
+
+
+
+if st.button("Show Recommend"):
+    movie_name, movie_poster = recommend(selectvalue)
+    col1,col2,col3,col4,col5=st.columns(5)
+    with col1:
+        st.text(movie_name[0])
+        st.image(movie_poster[0])
+    with col2:
+        st.text(movie_name[1])
+        st.image(movie_poster[1])
+    with col3:
+        st.text(movie_name[2])
+        st.image(movie_poster[2])
+    with col4:
+        st.text(movie_name[3])
+        st.image(movie_poster[3])
+    with col5:
+        st.text(movie_name[4])
+        st.image(movie_poster[4])
